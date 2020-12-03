c5a26a5da6621afc54f6aedf655ee752fd6cdcd5
Chris Mitchell <chris@delacour.co.nz>
2020-12-04 00:19:59 +1300

    Added prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 4,
+  semicolons: true,
+  printWidth: 135,
+
+  parser: "typescript",
+  semi: true,
+  arrowParens: "always",
+  jsxSingleQuote: false,
+};
